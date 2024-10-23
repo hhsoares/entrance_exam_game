f103088690e22aeafe14c24839c2352afa91a58f
Hugo Soares <hhsoares112@gmail.com>
2024-10-23 20:51:14 -0300

    score, press space to play

--- scenes/dino.gd
+++ scenes/dino.gd
@@ -6,14 +6,18 @@ const JUMP_SPEED : int = -1000
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta
 	if is_on_floor():
-		$RunCol.disabled = false
-		if Input.is_action_pressed("ui_accept"):
-			velocity.y = JUMP_SPEED
-		elif Input.is_action_pressed("ui_down"):
-			$AnimatedSprite2D.play("duck")
-			$RunCol.disabled = true
+		if not get_parent().game_running:
+			$AnimatedSprite2D.play("idle")
 		else:
-			$AnimatedSprite2D.play("run")
+			$RunCol.disabled = false
+			if Input.is_action_pressed("ui_accept"):
+				velocity.y = JUMP_SPEED
+			elif Input.is_action_pressed("ui_down"):
+				$AnimatedSprite2D.play("duck")
+				$RunCol.disabled = true
+			else:
+				$AnimatedSprite2D.play("run")
 	else:
 		$AnimatedSprite2D.play("jump")
+		
 	move_and_slide()
--- scenes/main.gd
+++ scenes/main.gd
@@ -4,10 +4,12 @@ const DINO_START_POS := Vector2i(61, 187)
 const CAM_START_POS := Vector2i(160, 120)
 
 var score : int
+const SCORE_MODIFIER : int = 10
 var speed  : float
 const START_SPEED  : float = 1
 const MAX_SPEED  : int = 5
 var screen_size : Vector2i
+var game_running : bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,23 +17,34 @@ func _ready():
 	new_game() # Replace with function body.
 
 func new_game():
-	
 	score = 0
+	show_score()
+	game_running = false
 	
 	$Dino.position = DINO_START_POS
 	$Dino.velocity = Vector2i(0, 0)
 	$Camera2D.position = CAM_START_POS
 	$Ground.position = Vector2i(0, 0)
 
+	$HUD.get_node("StartLabel").show()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	speed = START_SPEED
-	
-	$Dino.position.x += speed
-	$Camera2D.position.x += speed
-	
-	score += speed
-	print(score)
-	
-	if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
-		$Ground.position.x += screen_size.x
+	if game_running:
+		speed = START_SPEED
+		
+		$Dino.position.x += speed
+		$Camera2D.position.x += speed
+		
+		score += speed
+		show_score()
+		
+		if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
+			$Ground.position.x += screen_size.x
+	else:
+		if Input.is_action_pressed("ui_accept"):
+			game_running = true
+			$HUD.get_node("StartLabel").hide()
+
+func show_score():
+	$HUD.get_node("ScoreLabel").text = "SCORE: " + str(score / SCORE_MODIFIER)
