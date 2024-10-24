7514c0fe963a3a9c89475178bf396d0ad952cbb7
Hugo Soares <hhsoares112@gmail.com>
2024-10-23 21:22:50 -0300

    added obstacles, except bird

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,26 +1,39 @@
 extends Node
 
+var stump_scene = preload("res://scenes/stump.tscn")
+var rock_scene = preload("res://scenes/rock.tscn")
+var barrel_scene = preload("res://scenes/barrel.tscn")
+var bird_scene = preload("res://scenes/bird.tscn")
+var obstacle_types := [stump_scene, rock_scene, barrel_scene]
+var obstacles : Array
+var bird_heights := [100, 300]
+
 const DINO_START_POS := Vector2i(61, 187)
 const CAM_START_POS := Vector2i(160, 120)
-
+var difficulty
+const MAX_DIFFICULTY : int = 2
 var score : int
 const SCORE_MODIFIER : int = 10
 var speed  : float
-const START_SPEED  : float = 1
+const START_SPEED  : float = 2
 const MAX_SPEED  : int = 4
 const SPEED_MODIFIER : int = 800
 var screen_size : Vector2i
+var ground_height : int
 var game_running : bool
+var last_obs
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_window().size
+	ground_height = $Ground.get_node("Sprite2D").texture.get_height()
 	new_game() # Replace with function body.
 
 func new_game():
 	score = 0
 	show_score()
 	game_running = false
+	difficulty = 0
 	
 	$Dino.position = DINO_START_POS
 	$Dino.velocity = Vector2i(0, 0)
@@ -35,8 +48,9 @@ func _process(delta):
 		speed = START_SPEED + score / SPEED_MODIFIER
 		if speed > MAX_SPEED:
 			speed = MAX_SPEED
+		adjust_difficulty()
 		
-		print(speed)
+		generate_obs()
 		
 		$Dino.position.x += speed
 		$Camera2D.position.x += speed
@@ -51,5 +65,30 @@ func _process(delta):
 			game_running = true
 			$HUD.get_node("StartLabel").hide()
 
+func generate_obs():
+	if obstacles.is_empty() or last_obs.position.x < score + randi_range(30, 50):
+		var obs_type = obstacle_types[randi() % obstacle_types.size()]
+		var obs
+		var max_obs = difficulty + 1
+		for i in range(randi() % max_obs + 1):
+			obs = obs_type.instantiate()
+			var obs_height = obs.get_node("Sprite2D").texture.get_height()
+			var obs_scale = obs.get_node("Sprite2D").scale
+			var obs_x : int = screen_size.x + score + 100 + (i * 60)
+			var obs_y : int = screen_size.y - ground_height - (obs_height * obs_scale.y / 2) + 5
+			last_obs = obs
+			add_obs(obs, obs_x, obs_y)
+
+func add_obs(obs, x, y):
+	obs.position = Vector2(x, y)
+	add_child(obs)
+	obstacles.append(obs)
+
+
 func show_score():
 	$HUD.get_node("ScoreLabel").text = "SCORE: " + str(score / SCORE_MODIFIER)
+
+func adjust_difficulty():
+	difficulty = score / SPEED_MODIFIER
+	if difficulty > MAX_DIFFICULTY:
+		difficulty = MAX_DIFFICULTY
